f770ccb449d2c83bed3f79c6b2d3d0d7c9d62453
hacktivist123 <akintayoshedrack@gmail.com>
2020-09-23 19:16:23 +0100

    updated app

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Cloud Foundry' })
 })
 
+/* GET Test Page. */
+router.get('/test', function (req, res, next) {
+  res.render('test', { title: 'This is a Test Page' })
+})
+
 module.exports = router
